56a4cba4fee281f67fe60e7a2635ae9cc769800d
zoodogood <zoodogood@gmail.com>
2023-04-23 18:47:11 +0300

    Improve

--- src/core/database/mod.ts
+++ src/core/database/mod.ts
@@ -1,7 +1,14 @@
 import mongoose from "./deps.ts";
+import { default as IConfig } from "@config";
 
-export async function startApp(): Promise<void> {
-  await mongoose.connect("mongodb://localhost:27017");
+export async function startApp(config: typeof IConfig): Promise<void> {
+  const databaseConfig = config.database;
+  const needDebug = databaseConfig.debug;
+
+  await mongoose
+    .connect(databaseConfig.uri)
+    .then(() => needDebug && console.info("[DATABASE]", "is launched"))
+    .catch((error: Error) => needDebug && console.error("[DATABASE]", error));
 }
 
 export const mongooseApp = mongoose;
